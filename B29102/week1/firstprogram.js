e6d9082f6464ce0c0627ce86f4e17fe6664061fe
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-09-12 17:35:27 +0300

    KATUSIIME PRAISE MARTHA, B29102

---
+++ B29102/week1/firstprogram.js
@@ -0,0 +1,6 @@
+// THIS IS MY FIRST PROGRAM
+
+console.log("Hello World")
+console.log("B2102")
+console.log("Katusiime Praise Martha")
+console.log("BSIT")
---
+++ B29102/week1/variables.js
@@ -0,0 +1,27 @@
+//variables
+
+//innitialization
+var my_name = "Ndaebwa Joseph";
+
+//Declare a variable
+var height;
+
+// Assign a variable
+height = 3.4;
+
+
+//let
+let area = 90.5
+
+let f_name;
+f_name = "Joseph";
+
+//const
+const pie = 3.14;
+
+//assignment
+pie = 3.15;
+//concatnating
+console.log(my_name,"-",height)
+console.log("Area: ", area)
+console.log("Firstname,", f_name)
