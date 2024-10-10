7d11ecebc8543b498ea0ee078db632ae078561a0
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-10 18:02:55 +0300

    Add files via upload

---
+++ revision.js
@@ -0,0 +1,19 @@
+//difference between var, let and const
+// var is the oldest way to declare variables in Javascript
+//while let is a nwewer way to declare variables introduced in 2015 by ECMAScript
+//const is another newer way of declaring variables in javascript although it cannot be updated or redeclared
+
+//what happens if you try tob reasign varibles declared with let or const
+//with let, there are no issues, the value is reassigned
+//with const, you automatically get a 'Type error: assignment to constant variable'
+
+//var x = 10;
+//let y = 5;
+//const z = 3;
+
+// x = 20;
+// y = 15;
+//z = 7;
+//console.log(x, y, z);
+//The code behaves this way because the const variable has been reassigned
+
