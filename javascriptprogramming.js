d7c21905f60ba057259ec60ba8e8e668c40b1976
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-10 18:02:14 +0300

    Add files via upload

---
+++ javascriptprogramming.js
@@ -0,0 +1,36 @@
+//AN if statement is used in javascript to check if a block of code
+//corresponds with what is needed
+//An if statement is used to compare and contrast two conditions to get the most
+// right condition according to the required statement
+
+//let temperature = 30;
+//if (temperature > 25) {       
+//    console.log('Its hot!');
+//} else {
+//  console.log('Its cold');
+//}
+//I would modify the program by adding elseif in order for it to  print
+//a message for temperatures between 15 and 25 degrees
+//like this
+
+let temperature = 30;
+if (temperature > 25) {
+    console.log('Its hot!');
+} else if (temperature > 15 && temperature <25){
+    console.log('Its moderate');
+}else{
+    console.log('Its cold')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
