0f52066bcab99aa6e22fbe0e4636507d24117604
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-10 18:03:31 +0300

    Add files via upload

---
+++ revison3.js
@@ -0,0 +1,10 @@
+let age = 18;
+if (age >= 18) {
+    if (age < 65) {
+        console.log('You are of retirement age.');
+    }else if (age <65 && age >20) {
+        console.log("You are eligible to work ");
+    }
+}else {
+    console.log('You are too young to work.')
+}
