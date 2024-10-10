3ac7e78ca0fab8a2f21514bd14cddaafe94f5f27
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-10 18:03:15 +0300

    Add files via upload

---
+++ revision2.js
@@ -0,0 +1,5 @@
+//A simple Javascript program that declares a variable to store the name of a student and a constant 
+//for the total number of students in an IT class.
+let sname = "Katusiime Praise Martha";
+const no_of_students = 89;
+console.log(`${sname} is one of the ${no_of_students} students doing IT`)
