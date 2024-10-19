5fd4664c69552d5002e469ddb803395ec6b62325
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-19 22:08:12 +0300

    Add files via upload

---
+++ B1.js
@@ -0,0 +1,11 @@
+//Explained below is the role of an integrated development enironment in Javascript programming
+
+//An integrated development enironment(IDE) in Javascript provides an environment
+//to create authentic code
+//An IDE also makes it easy for programmers to debug their codes
+//It also makes it easy for programmers to interact with AI tools as they carry out thei coding
+//It provides an environment for code to run and produce an output  
+
+//Below are the most popular examples of IDEs used in Javascript
+//-Visual Studio Code
+//-Node.js
