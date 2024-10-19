efc8ad1a27f40f5d927a30638129745da182eb46
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-19 22:07:14 +0300

    Add files via upload

---
+++ A2.js
@@ -0,0 +1,6 @@
+//A javascript program that writes the name of the country as Africa and 
+//its population using variables and the current year as a constant
+let country = "Madagascar"
+let population = 99900000 //million people
+const currentYear = 2024
+console.log(`The country in Africa, ${country}, has a population of ${population} in this current year of ${currentYear}!`)
---
+++ A3.js
@@ -0,0 +1,9 @@
+//Difference between var, let and const and what happens when you rerassign these variables
+
+
+//var differenciates from let and const as it is an older way of declaring variables and 
+//not encouraged most times wheareas let and const are a newer ways of declaring variables
+//wheareas Const differnciates from let and var as it is assigned to variables that cannot be reassigned
+
+//Var and Let can be reassigned and bring out the right and clear value in the console whereas 
+//Const brings a 'value error' when reassigned
