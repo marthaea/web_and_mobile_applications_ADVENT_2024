39b8de2c53f751f57eeffc27502166c0a5c22821
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-19 22:08:35 +0300

    Add files via upload

---
+++ B2.js
@@ -0,0 +1,6 @@
+// Steps of installing Javascript using Visual Studio Code
+//Step one; Download visual studio code
+//Step two; Download node.js
+//step three; set up node in visual studio code
+//step four; download the necessary extentions for javascript
+//step five; activate node using the command prompt
