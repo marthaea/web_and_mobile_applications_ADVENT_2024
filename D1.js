933d61a5fd076fab94cdee23713e156ed0cf4482
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-24 21:36:22 +0300

    Add files via upload

---
+++ D1.js
@@ -0,0 +1,13 @@
+let MTN = 1 //minutes
+switch(MTN) {
+    case 1:
+        console.log("MTN");
+        break;
+    case 2:
+        console.log("Airtel");
+        break;
+    case 3:
+        console.log("Africell")
+    default:
+        console.log("Unrecognized")
+}
