2379722023fab423ea879177e13b7c14d6b5a4a5
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-10-24 21:37:39 +0300

    Add files via upload

---
+++ G2.js
@@ -0,0 +1,8 @@
+
+function greetUser(user1, user2, user3) {
+    console.log(`Welcome to Africa! ${user1}`),
+    console.log(`Welcome to Africa! ${user2}`),
+    console.log(`Welcome to Africa! ${user3}`)
+}
+
+console.log(greetUser(Martha, Kiara, Grace))
